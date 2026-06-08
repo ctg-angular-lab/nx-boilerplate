e114d0bbfecbece3d18675215e611761c79011b4
Tbars84 <camilo.tabares.84@gmail.com>
2026-06-08 08:10:17 -0500

    Configuracion Inicial del Boilerplate

---
+++ libs/ui-charts/README.md
@@ -0,0 +1,3 @@
+# ui-charts
+
+This library was generated with [Nx](https://nx.dev).
---
+++ libs/ui-charts/src/lib/ui-charts/ui-charts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui-charts',
+  imports: [],
+  templateUrl: './ui-charts.html',
+  styleUrl: './ui-charts.scss',
+})
+export class UiCharts {}
