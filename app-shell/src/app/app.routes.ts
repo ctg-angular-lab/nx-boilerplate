5f0cb83d48780fc2ab9c8a54aebc4d15b3f31dca
Tbars84 <camilo.tabares.84@gmail.com>
2026-06-08 12:25:10 -0500

    Feat: succesfully integrated angular material theme in app-shell

--- app-shell/src/app/app.routes.ts
+++ app-shell/src/app/app.routes.ts
@@ -1,9 +1,4 @@
-import { NxWelcome } from './nx-welcome';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  {
-    path: '',
-    component: NxWelcome,
-  },
 ];
--- app-shell/src/app/app.ts
+++ app-shell/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [ MatButtonModule, MatDividerModule, MatIconModule, RouterModule ],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
