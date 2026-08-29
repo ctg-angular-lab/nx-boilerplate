ad3942377d08d07b86cf0d3cb3bc9ee63e1efcbc
Tbars84 <camilo.tabares.84@gmail.com>
2026-08-29 09:52:24 -0500

    Rorganizar apps y librerias para un mfe modular

--- app-shell/src/app/app.routes.ts
+++ app-shell/src/app/app.routes.ts
@@ -1,4 +1,19 @@
 import { Route } from '@angular/router';
+import { loadRemote } from '@module-federation/enhanced/runtime';
 
 export const appRoutes: Route[] = [
+  {
+    path: 'Login',
+    loadChildren: () =>
+      loadRemote<typeof import('Login/Routes')>('Login/Routes').then(
+        (m) => m!.remoteRoutes,
+      ),
+  },
+  {
+    path: 'Dashboard',
+    loadChildren: () =>
+      loadRemote<typeof import('Dashboard/Routes')>('Dashboard/Routes').then(
+        (m) => m!.remoteRoutes,
+      ),
+  },
 ];
